cbb40ac63604501bee56e7472ba39bce329f21f6
Brad Wilson <brad.n.wilson@live.com>
2019-10-08 15:08:46 -0400

    Started controller and cleaned up view folder

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,34 +1,62 @@
-class Review{ 
+// Tags, Cat need relationship with Tag & Category table
+module.exports = (sequelize, type) => {
+    return sequelize.define('reviews', {
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text: { 
+            type: STRING,
+            allowNull: false
+        },
+        reviewItem: {
+          type: STRING,
+          allowNull: false
+        },
+        author: {
+            type: STRING,
+            allowNull: false
+        },
+        imgUrl: type.STRING,
+        tags: type.STRING,
+        category: type.STRING
+    })
+}
 
-    constructor(props){
-        this.author = props.author; 
-        this.reviewBody = props.review;
-        this.reviewItem = props.reviewItem;
-        this.tags = props.tags;
-    }
-    setAuthor(writer){
-        this.author = writer
-    };
-    setReview(words){
-        this.reviewBody = words
-    };
-    setReviewItem(item){
-        this.reviewItem = item
-    };
-    setTags(hashtag){
-        this.tags = hashtag
-    };
-    getAuthor(){
-        return this.author;
-    };
-    getReview(){
-        return this.reviewBody;
-    };
-    getReviewItem(){
-        return this.reviewItem;
-    };
-    getTags(){
-        return this.tags;
-    };
-};
-module.exports = Review;
+
+
+// class Review{ 
+
+//     constructor(props){
+//         this.author = props.author; 
+//         this.reviewBody = props.review;
+//         this.reviewItem = props.reviewItem;
+//         this.tags = props.tags;
+//     }
+//     setAuthor(writer){
+//         this.author = writer
+//     };
+//     setReview(words){
+//         this.reviewBody = words
+//     };
+//     setReviewItem(item){
+//         this.reviewItem = item
+//     };
+//     setTags(hashtag){
+//         this.tags = hashtag
+//     };
+//     getAuthor(){
+//         return this.author;
+//     };
+//     getReview(){
+//         return this.reviewBody;
+//     };
+//     getReviewItem(){
+//         return this.reviewItem;
+//     };
+//     getTags(){
+//         return this.tags;
+//     };
+// };
+// module.exports = Review;
