ce93c88c3fc097b96638ddad73b4a317d1df0c9d
Brad Wilson <brad.n.wilson@live.com>
2019-10-10 15:11:43 -0400

    Failed testing but succeeded in styling so hard

--- src/controllers/review-controller.test.js
+++ src/controllers/review-controller.test.js
@@ -1,26 +1,24 @@
 const reviewService = require("../services/review-service");
 const ReviewController = require("./review-controller");
 
-// static renderAll(req, res) {
-//     res.render("students/all", { students: studentService.findAll() });
-// }
-
-describe("rerenderAll", () => {
-  test.skip("should call res.render with '/' and a valid model object", () => {
-    // Arrange
-    const requestMock = {};
-    const responseMock = {
-      render: jest.fn()
-    };
-    reviewService.findAll = jest.fn(() => []);
-
-    // Act
-    ReviewController.renderAll(requestMock, responseMock);
-
-    // Assert
-    expect(responseMock.render).toHaveBeenCalledTimes(1);
-    expect(responseMock.render).toHaveBeenCalledWith("/", {
-      reviews: []
+describe("ReviewController", () => {
+    describe("renderAll", () => {
+      test.skip("should call res.render with '/' and a valid model object", () => {
+        // Arrange
+        const requestMock = {}; // a blank request...
+        const responseMock = {  // a mock function response...
+          render: jest.fn()
+        };
+        reviewService.findAll = jest.fn(() => []);  // reviewService should call a mock function to return an array
+    
+        // Act
+        ReviewController.renderAll(requestMock, responseMock);
+    
+        // Assert
+        expect(responseMock.render).toHaveBeenCalledTimes(1);
+        expect(responseMock.render).toHaveBeenCalledWith("index", {
+          reviews: []
+        });
+      });
     });
-  });
 });
