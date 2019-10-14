1e41813b0490d14507da18c1d75f3bf046ff1e16
Brad Wilson <brad.n.wilson@live.com>
2019-10-14 11:42:24 -0400

    Changed Review service and controller methods to use async/await

--- src/controllers/review-controller.js
+++ src/controllers/review-controller.js
@@ -3,31 +3,45 @@ const reviewService = require('../services/review-service');
 const Review = require('../models/Review.sequelize');
 
 class ReviewController { 
-    static renderAll (req, res) {
-        reviewService.findAll((reviews) => {
-            console.log(reviews);
-            res.render("index", { reviews: reviews })
-        })
-    }
-    static renderReview (req, res) {
-        const id = req.params.id;
+    // static renderAll (req, res) {
+    //     reviewService.findAll((reviews) => {
+    //         console.log(reviews);
+    //         res.render("index", { reviews: reviews })
+    //     })
+    // }
+    static async renderAll (req, res) {
+        try {
+            const reviews = await reviewService.findAll();
+            res.render("index", {reviews: reviews});
 
-        reviewService.findReview(id, (review) => {
-            console.log(review);
-            res.render("review", { review : review });
-        })
+        } catch (error) {
+            res.render("error", {error: error});
+        }
     }
-    static addReview (req, res) {
+
+    static async renderReview (req, res) {
+        const id = req.params.id;
+        try{
+            const review = await reviewService.findReview(id) 
+            res.render("review", { review : review });
+        } catch (error) {
+            res.render("error", {error: error} );
+        }
+    }
+
+    static async addReview (req, res) {
         const author = req.body.author;
         const reviewBody = req.body.reviewBody;
         const reviewItem = req.body.reviewItem;
         const category = req.body.category;
-    
-        reviewService.save(new reviewDomainObject(author, reviewBody, reviewItem, category), (newReview) => {
-            console.log(newReview);
+        try{
+            await reviewService.save(new reviewDomainObject(author, reviewBody, reviewItem, category));
             res.redirect("/");
-        })
-    }
+        } catch(error) {
+            res.render("error", {error: error});
+        }
+    }     
 }
+
 
 module.exports = ReviewController;
