9143379c1d42aba874a689f4e080f3cb5f2fc377
Zack Kramer <zckramer83@gmail.com>
2019-10-15 14:43:02 -0400

    little bit o progress on DB tables

--- src/util/db-base-content.js
+++ src/util/db-base-content.js
@@ -1,15 +1,15 @@
 // var TagsTable = require('./src/services/tag-service');
-var LocationsTable = require('../models/Location.sequelize');
+const LocationsTable = require('../models/Location.sequelize');
 
 // DB BullShit base data
-const baseLocationsList = [ {name: "McDonald's"}, {name: "Starbuck's"}, {name: "Stauf's"}, {name: "Crimson Cup"}, {name: "Timmy Ho's"} ]
-const baseTagList = [ {name: "Decaffeinated"}, {name: "Caffeinated"}, {name: "Hot"}, {name: "Cold"} ]
+const baseLocationsList = [ {location: "McDonald's"}, {location: "Starbuck's"}, {location: "Stauf's"}, {location: "Crimson Cup"}, {location: "Timmy Ho's"} ]
+// const baseTagList = [ {name: "Decaffeinated"}, {name: "Caffeinated"}, {name: "Hot"}, {name: "Cold"} ]
 
 // set up BullShit Locations and Tags table
 const buildBaseData = () => {
     LocationsTable.bulkCreate(baseLocationsList, {
-      fields: ["name"],
-      updateOnDuplicate: ["name"]
+      fields: ["location"],
+      updateOnDuplicate: ["location"]
     })
 }
 
